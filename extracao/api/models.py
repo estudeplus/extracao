266ee8b05b9d63491b209a6114d1ec013f4e81e4
Bruno Rdrigues Santos <bruesmanbruesman@hotmail.com>
2019-05-25 10:48:43 -0300

    feat#3 added student model

--- extracao/api/models.py
+++ extracao/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+
+    code = models.CharField(
+        ('Code'),
+        help_text=("Student Code"),
+        max_length=15,
+        primary_key=True
+    )
+
+    name = models.CharField(
+        ('Name'),
+        help_text=("Student Name"),
+        max_length=100,
+    )
+
+    ira = models.FloatField(
+        ('IRA'),
+    )
+
+    grade = models.FloatField(
+        ('Grade'),
+    )
+
+    mention = models.CharField(
+        ('Mention'),
+        help_text=("Student Mention in the subject"),
+        max_length=2,
+    )
