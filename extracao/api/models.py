94c955f47cd61edffc558e0d6c80bd90789b7db4
Bruno Rdrigues Santos <bruesmanbruesman@hotmail.com>
2019-05-25 11:22:55 -0300

    feat#3 added professor model

--- extracao/api/models.py
+++ extracao/api/models.py
@@ -40,3 +40,29 @@ class Student(models.Model):
         """
         verbose_name = ("Student")
         verbose_name_plural = ("Students")
+
+
+class Professor(models.Model):
+
+    id = models.AutoField(
+        primary_key=True
+    )
+
+    name = models.CharField(
+        ('Name'),
+        help_text=("Professor Name"),
+        max_length=100,
+    )
+
+    name = models.CharField(
+        ('Email'),
+        help_text=("Professor email"),
+        max_length=100,
+    )
+
+    class Meta:
+        verbose_name = ("Professor")
+        verbose_name_plural = ("Professors")
+
+    def __str__(self):
+        return self.name
