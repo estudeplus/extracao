4fee9f76b1a201425aca7b47811f484531ff1071
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-05-27 12:39:44 -0300

    feat #12 added methods to get the state of the core manager

--- extracao/api/apimanager.py
+++ extracao/api/apimanager.py
@@ -1,11 +1,13 @@
-from ..core.models import Document
 from .models import Professor, Subject, Student
 
 class ApiManager():
 
     def __init__(self):
         self._subject = None
-        self._state = None
+        self._data = None
+    
+    def set_subject(self, subject):
+        self._subject = subject
     
     def update(self):
-        pass
+        self._data = self._subject.get_state()
--- extracao/core/coremanager.py
+++ extracao/core/coremanager.py
@@ -1,8 +1,19 @@
+from .models import Document
+from ..api.apimanager import ApiManager
+
+
 class CoreManager():
 
-    def __init__(self):
+    def __init__(self, state):
         self._observer = None
-        self._subject_state = None
+        self._state = state
+    
+    def attach(self, observer):
+        observer._subject = self
+        self._observer = observer
+    
+    def get_state(self):
+        return self._state.document
     
     def _notify(self):
         pass
